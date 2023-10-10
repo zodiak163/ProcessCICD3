607ba1b7097d1477147e91e9b3a475c7cbe070b9
Roman Nor <keigh@yandex.ru>
2023-10-10 16:34:12 +0400

    test

---
+++ src/main/java/plaindoll/Welcomer.java
@@ -0,0 +1,16 @@
+package plaindoll;
+
+public class Welcomer{
+	public String sayWelcome() {
+		return "Welcome home, good hunter. What is it your desire?";
+	}
+	public String sayFarewell() {
+		return "Farewell, good hunter. May you find your worth in waking world.";
+	}
+	public String sayNeedGold(){
+		return "Not enough gold";
+	}
+	public String saySome(){
+		return "something in the way";
+	}
+}
