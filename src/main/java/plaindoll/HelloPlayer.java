6a9078e25c3df7a1441b28cde2a43a5a11910225
zodiak <keigh@yandex.ru>
2024-02-06 11:20:04 +0400

    2.6

--- src/main/java/plaindoll/HelloPlayer.java
+++
@@ -1,9 +0,0 @@
-package plaindoll;
-
-public class HelloPlayer{
-	public static void main(String[] args) {
-		Welcomer welcomer = new Welcomer();
-		System.out.println(welcomer.sayWelcome());
-		System.out.println(welcomer.sayFarewell());
-	}
-}
